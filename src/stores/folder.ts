f2ea89e5767d81f28d6583dbfe929298642ea135
Daniel Lima <danielmlimag@gmail.com>
2025-08-15 22:21:18 +0200

    fix: use setup syntax to allow nesting stores

--- src/stores/folder.ts
+++ src/stores/folder.ts
@@ -1,16 +1,14 @@
 import type { Folder } from '@/types/types'
 import { defineStore } from 'pinia'
 import { getFolders } from '@/services/folder-service'
+import { ref } from 'vue'
 
-export const useFolderStore = defineStore('folder', {
-  state: () => ({
-    folders: getFolders(),
-    selectedFolder: {} as Folder,
-  }),
-  getters: {},
-  actions: {
-    selectFolder(folder: Folder) {
-      this.selectedFolder = folder
-    },
-  },
+export const useFolderStore = defineStore('folder', () => {
+  const folders = ref(getFolders())
+  const selectedFolder = ref({} as Folder)
+  function selectFolder(folder: Folder) {
+    selectedFolder.value = folder
+  }
+
+  return { folders, selectedFolder, selectFolder }
 })
--- src/stores/note.ts
+++ src/stores/note.ts
@@ -2,18 +2,16 @@ import type { Note } from '@/types/types'
 import { defineStore } from 'pinia'
 import { useFolderStore } from '@/stores/folder'
 import { getNotesInFolder } from '@/services/note-service'
+import { ref } from 'vue'
 
-const folderStore = useFolderStore()
+export const useNoteStore = defineStore('note', () => {
+  const folderStore = useFolderStore()
 
-export const useNoteStore = defineStore('note', {
-  state: () => ({
-    notes: getNotesInFolder(folderStore.selectedFolder.id),
-    selectedNote: {} as Note,
-  }),
-  getters: {},
-  actions: {
-    selectNote(note: Note) {
-      this.selectedNote = note
-    },
-  },
+  const notes = ref(getNotesInFolder(folderStore.selectedFolder.id))
+  const selectedNote = ref({} as Note)
+  function selectNote(note: Note) {
+    selectedNote.value = note
+  }
+
+  return { notes, selectedNote, selectNote }
 })
